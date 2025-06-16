a6cd46dea2c0d095a4bcaa29ff6cb0c507192be6
SRIRAM P <165500350+Sriramofficialsit@users.noreply.github.com>
2025-06-16 22:06:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const corsOptions = {
 
 
 go_rental.options("*", cors(corsOptions));
-go_rental.use("*",cors());
+
 go_rental.use(Express.json());
 go_rental.use("/auth", Auth);
 go_rental.use("/dashboard", Dashboard);
